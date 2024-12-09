58057467d16a39fd153a16701bc1ebb4dac8bd57
Andrew Jorgensen <git@aj.codes>
2024-12-09 16:05:14 -0500

    feat: basic hello world chi

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	fmt.Println("Hello, world!")
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world!"))
+	})
+
+	log.Printf("Listening on port 3333")
+	http.ListenAndServe(":3333", r)
 }
